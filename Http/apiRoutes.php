99d35c1fe682b18ced81027023b251f2570384d2
Omar Chunza <odchunzag.une@gmail.com>
2023-09-27 11:31:47 -0500

    Actualizacion de modulo IcommerceWompi a laravel 10.

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Routing\Router;
 
-$router->group(['prefix' => 'icommercewompi'], function (Router $router) {
-    
+Route::prefix('icommercewompi')->group(function (Router $router) {
     $router->get('/', [
         'as' => 'icommercewompi.api.wompi.init',
         'uses' => 'IcommerceWompiApiController@init',
@@ -13,5 +12,4 @@ $router->group(['prefix' => 'icommercewompi'], function (Router $router) {
         'as' => 'icommercewompi.api.wompi.confirmation',
         'uses' => 'IcommerceWompiApiController@confirmation',
     ]);
-
 });
