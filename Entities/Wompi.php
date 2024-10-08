12d43df2f0ec432edea2bc58c227692576b61eab
William <williamverde10@gmail.com>
2024-10-08 16:00:57 -0400

    Update Wompi Entity attributes to public

--- Entities/Wompi.php
+++ Entities/Wompi.php
@@ -6,65 +6,55 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wompi
 {
-    private $urlAction;
 
-    private $publicKey;
+	  public $urlAction; 
+  	public $publicKey;
+    public $currency;
+  	public $referenceCode;
+  	public $amount;
+  	public $redirectUrl;
+    public $signatureIntegrity;
+  	
+  	private $_htmlFormCode;
+  	private $_htmlCode;
+  	private $nameForm;
 
-    private $currency;
+  	function __construct($url='',$publicKey='',$referenceCode='',$amount=0,$currency='',$redirectUrl=''){
 
-    private $referenceCode;
-
-    private $amount;
-
-    private $redirectUrl;
-
-    private $_htmlFormCode;
-
-    private $_htmlCode;
-
-    private $nameForm;
-
-    public function __construct($url = '', $publicKey = '', $referenceCode = '', $amount = 0, $currency = '', $redirectUrl = '')
-    {
         $this->setReferenceCode($referenceCode);
         $this->setAmount($amount);
         $this->setPublicKey($publicKey);
         $this->setUrlgate($url);
         $this->setCurrency($currency);
         $this->setRedirectUrl($redirectUrl);
-    }
-
-  public function setUrlgate($url)
-  {
-      $this->urlAction = $url;
+  
   }
 
-  public function setPublicKey($publicKey)
-  {
-      $this->publicKey = $publicKey;
+  public function setUrlgate($url){
+    $this->urlAction=$url;
   }
 
-  public function setCurrency($currency)
-  {
-      $this->currency = $currency;
+  public function setPublicKey($publicKey){
+    $this->publicKey = $publicKey;
   }
 
-  public function setReferenceCode($referenceCode)
-  {
-      $this->referenceCode = $referenceCode;
+  public function setCurrency($currency){
+    $this->currency = $currency;
+  }
+
+  public function setReferenceCode($referenceCode){
+  	    $this->referenceCode = $referenceCode;
   }
 
   /**
-   * Amount in Cents
-   */
-  public function setAmount($amount)
-  {
-      $this->amount = $amount * 100;
+  * Amount in Cents
+  */
+  public function setAmount($amount){
+  	   $this->amount = $amount * 100;
   }
-
-  public function setRedirectUrl($redirectUrl)
-  {
-      $this->redirectUrl = $redirectUrl;
+  
+  public function setRedirectUrl($redirectUrl){
+  	   $this->redirectUrl =$redirectUrl;
   }
 
   public function setSignatureIntegrity($signature){
@@ -72,26 +62,26 @@ class Wompi
   }
 
   /**
-   * FORM - Set Form Name
-   */
+  * FORM - Set Form Name
+  */
   public function setNameForm($name = 'payForm')
   {
-      $this->nameForm = $name;
+        $this->nameForm = $name;
   }
 
   /**
-   * FORM - Add input
-   */
+  * FORM - Add input
+  */
   private function _addInput($string, $value)
   {
-      return '<input type="hidden" name="'.$string.'" value="'.htmlentities($value, ENT_COMPAT, 'UTF-8').'"/>'."\n";
+        return '<input type="hidden" name="' .$string. '" value="' . htmlentities($value, ENT_COMPAT, 'UTF-8') . '"/>' . "\n";
   }
 
   /**
   * FORM - Add make fields
   */
   public function _makeFields(){
-
+  
     $this->_htmlFormCode.=$this->_addInput('public-key',$this->publicKey);
     $this->_htmlFormCode.=$this->_addInput('currency',$this->currency);
     $this->_htmlFormCode.=$this->_addInput('amount-in-cents',$this->amount);
@@ -100,25 +90,26 @@ class Wompi
     $this->_htmlFormCode.=$this->_addInput('redirect-url',$this->redirectUrl);
 
   }
-
+  
   /**
-   * FORM - Make Form
-   */
+  * FORM - Make Form
+  */
   private function _makeForm()
   {
-      $this->_htmlCode .= '<form action="'.$this->urlAction.'" method="GET" id="'.$this->nameForm.'" name="'.$this->nameForm.'"/>'."\n";
-      $this->_htmlCode .= $this->_htmlFormCode;
-  }
+        $this->_htmlCode .= '<form action="' . $this->urlAction . '" method="GET" id="'.$this->nameForm.'" name="'.$this->nameForm.'"/>' . "\n";
+        $this->_htmlCode .=$this->_htmlFormCode;
 
+  }
+ 
   /**
-   * FORM - Render
-   */
+  * FORM - Render
+  */
   public function renderPaymentForm()
   {
-      $this->setNameForm();
+  		$this->setNameForm();
 
       $time = time();
-      error_log('---Payment page sampledan gelen loglar---'.$time, 0);
+      error_log("---Payment page sampledan gelen loglar---".$time,0);
 
       $this->_makeFields();
       $this->_makeForm();
@@ -127,11 +118,13 @@ class Wompi
   }
 
   /**
-   * Execute Redirection
-   */
+  * Execute Redirection
+  */
   public function executeRedirection()
   {
-      echo $this->renderPaymentForm();
-      echo '<script>document.forms["'.$this->nameForm.'"].submit();</script>';
+    echo $this->renderPaymentForm();
+    echo '<script>document.forms["'.$this->nameForm.'"].submit();</script>';
   }
+	
+
 }
