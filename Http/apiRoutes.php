b4d54f4657431b756a7aed3b0e2b22abb3fe11e7
William <williamverde10@gmail.com>
2024-11-08 16:01:19 -0400

    Added: Routes to API to payment sources

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -26,5 +26,29 @@ Route::prefix('icommercewompi')->group(function (Router $router) {
         'as' => 'icommercewompi.api.wompi.simulateRecurrence',
         'uses' => 'IcommerceWompiApiController@simulateRecurrence',
     ]);
+
+
+    /**
+     * Routes to Crud
+     */
+    Route::prefix('/v1')->group(function (Router $router) {
+
+        $router->apiCrud([
+            'module' => 'icommercewompi',
+            'controller' => 'PaymentSourcesApiController',
+            'prefix' => 'payment-sources',
+            'permission' => 'icommercewompi.paymentsources',
+            //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
+            // 'customRoutes' => [ // Include custom routes if needed
+            //  [
+            //    'method' => 'post', // get,post,put....
+            //    'path' => '/some-path', // Route Path
+            //    'uses' => 'ControllerMethodName', //Name of the controller method to use
+            //    'middleware' => [] // if not set up middleware, auth:api will be the default
+            //  ]
+            // ]
+        ]);
+    
+    });
     
 });
